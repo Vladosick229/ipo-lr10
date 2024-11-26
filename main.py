6fe9b19a712d3dfa0070142175a6b3a843acb806
Vlad <vladworking5@gmail.com>
2024-11-26 17:50:30 +0300

    file .json created

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
 import json
 from bs4 import BeautifulSoup as bs
+quotes = []
+url = "http://quotes.toscrape.com/"
+while url:
+    response = requests.get(url)
+    soup = bs(response.text, "html.parser")
+    for quote in soup.find_all("div", class_="quote"):
+        text = quote.find("span", class_="text").get_text()
+        author = quote.find("small", class_="author").get_text()
+        quotes.append({"quote": text, "author": author})
+    next_button = soup.find("li", class_="next")
+    url = next_button.find("a")["href"] if next_button else None
+    url = "http://quotes.toscrape.com" + url if url else None
 
-URL="https://quotes.toscrape.com/"
+def print_quotes(quotes):
+    for idx, quote in enumerate(quotes, 1):
+        print(f'{idx}. Quote: {quote["quote"]}; Author: {quote["author"]};')
 
-quotes=[]
-response=requests.get(URL)
-soup = bs(response.text, "html.parser")
-for quote in soup.find_all("div",class_="quote"):
-    text=quote.find_all("span",class_="text").get_text()
-    author=quote.find_all("small",class_="author").get_text()
-    quotes.append({"quote":text,"author":author})
-next=soup.find("li",class_="next")
-url=next.find("a")["href"]
-url
+print_quotes(quotes)
+
+def save_to_json(quotes,file="data.json"):
+    with open(file,"w",encoding="utf-8")as f:
+        json.dump(quotes, f, ensure_ascii=False, indent=4)
+
+save_to_json(quotes)
